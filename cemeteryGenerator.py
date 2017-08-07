e447ab8de8e5f8daeb7e634e9bc9c835e5aab022
Izaac <izaac.kirscht@gmail.com>
2017-08-07 16:22:27 -0500

    generate visual representation of death toll.
    Randomized headstones.

---
+++ cemeteryGenerator.py
@@ -0,0 +1,8 @@
+from random import randint
+
+for x in xrange(2537):
+	with open('cemetary.html', 'a+') as graves:
+		grave = '<img src="gravestones%s.png" width="1%%" height="2%%"> ' % randint(0, 11)
+		graves.write(grave)
+		print grave
+		print x
